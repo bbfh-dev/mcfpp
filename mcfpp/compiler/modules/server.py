2c46fc69fbca0887f75f4dcd2607dbfff81a7213
BubbleFish <bbfh-dev@github.com>
2023-10-16 17:07:05 +0300

    Add basic Server module compilation (paths only)

--- mcfpp/compiler/modules/server.py
+++ mcfpp/compiler/modules/server.py
@@ -8,4 +8,11 @@ class Server(BaseModule):
 
     @override
     async def build(self):
-        return Node("(namespace)")
+        for name, fn in self.methods.items():
+            namespace, paths = self.file(name).split(":")[0], path(
+                "functions", self.file(name).split(":")[1]
+            )
+            self.tree.name = namespace
+            self.tree.add_branch(paths)
+            # self.tree.as_branch(paths).file.add("say Hi")
+        return self.tree
