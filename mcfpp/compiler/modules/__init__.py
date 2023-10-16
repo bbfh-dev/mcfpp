39423aedf4c1bc11c19178257aca39426cd01327
BubbleFish <bbfh-dev@github.com>
2023-10-16 16:52:48 +0300

    Add requires() module decorator

--- mcfpp/compiler/modules/__init__.py
+++ mcfpp/compiler/modules/__init__.py
@@ -3,3 +3,27 @@ from .entity import Entity
 from .player import Player
 from .score import Score
 from .server import Server
+
+
+class Empty(Server):
+    pass
+
+
+def requires(*modules: type[BaseModule]):
+    """
+    Class decorator that prevents a module from being compiled
+    if the modules specified in it are not used in the pipeline.
+
+    Useful to ensure no unused code is produced.
+    """
+
+    def wrap(cls):
+        def call(*args, **kwargs):
+            for module in modules:
+                if not kwargs.get("fn_has_module")(module.__name__):
+                    return Empty(*args, **kwargs)
+            return cls(*args, **kwargs)
+
+        return call
+
+    return wrap
