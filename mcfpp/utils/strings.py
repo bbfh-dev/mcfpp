bd56e7c49ed6d6044f65a9c9c842ac43cd59b635
BubbleFish <bbfh-dev@github.com>
2023-10-16 16:52:05 +0300

    Add path splitting

--- mcfpp/utils/strings.py
+++ mcfpp/utils/strings.py
@@ -12,3 +12,11 @@ def format_string(string: str, pattern: str, replacements: list[str]):
 
 def is_empty(string: str):
     return len(string) == 0
+
+
+def path(*paths: str):
+    return "/".join(paths)
+
+
+def split_path(paths: str):
+    return [i for i in re.split(r"[/:]", paths) if i]
